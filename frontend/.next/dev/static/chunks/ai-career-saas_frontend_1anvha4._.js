c160896fec0a550bb91ff34d41da0f09e31e5df9
Bhagya Sree Malla <bhagyasreemallaone@gmail.com>
2026-06-03 16:59:08 +0530

    remove .next build cache and fix gitignore

--- frontend/.next/dev/static/chunks/ai-career-saas_frontend_1anvha4._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_0jom9hr._.js","static/chunks/1rj__next_dist_compiled_next-devtools_index_01qs9hn.js","static/chunks/1rj__next_dist_compiled_react-dom_1jobqap._.js","static/chunks/1rj__next_dist_compiled_react-server-dom-turbopack_0ebc_mn._.js","static/chunks/1rj__next_dist_compiled_0us4_y0._.js","static/chunks/1rj__next_dist_client_0ylqhec._.js","static/chunks/1rj__next_dist_140-cso._.js","static/chunks/1rj__@swc_helpers_cjs_1h8juk4._.js"],
-    source: "entry"
-});
--- frontend/.next/dev/static/chunks/ai-career-saas_frontend_app_page_tsx_05h-nbt._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/ai-career-saas_frontend_023i0y6._.js"],
-    source: "dynamic"
-});
