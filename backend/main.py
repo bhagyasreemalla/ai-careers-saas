4c6629093e388adafb7507581d57e9d15865c26b
Malla Bhagya Sree <bhagyasreemalla@users.noreply.github.com>
2026-06-07 22:35:49 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://ai-career-saas.vercel.app",
+        "https://ai-careers-saas.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
